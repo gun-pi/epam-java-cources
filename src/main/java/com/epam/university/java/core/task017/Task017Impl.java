03e6a9ec548091ce8ecff3615b65fec0dd9ae1c8
Pavel <gunpavel.i@gmail.com>
2020-09-03 09:18:45 +0300

    some changes in Task017

--- src/main/java/com/epam/university/java/core/task017/Task017Impl.java
+++ src/main/java/com/epam/university/java/core/task017/Task017Impl.java
@@ -1,6 +1,7 @@
 package com.epam.university.java.core.task017;
 
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
 
 public class Task017Impl implements Task017 {
@@ -12,6 +13,16 @@ public class Task017Impl implements Task017 {
      */
     @Override
     public String formatString(Object... args) {
+        if (args == null || args.length == 0) {
+            throw new IllegalArgumentException();
+        }
+
+        for (Object each : args) {
+            if (each == null) {
+                throw new IllegalArgumentException();
+            }
+        }
+
         return String.format("You know %s, %s!", args[0], args[1]);
     }
 
@@ -23,6 +34,16 @@ public class Task017Impl implements Task017 {
      */
     @Override
     public String formatNumbers(Object... args) {
+        if (args == null || args.length == 0) {
+            throw new IllegalArgumentException();
+        }
+
+        for (Object each : args) {
+            if (each == null) {
+                throw new IllegalArgumentException();
+            }
+        }
+
         String bin = Double.toHexString(Double.parseDouble(args[0].toString()));
         //bin =  String.format("%016d", Double.parseDouble(bin));
         return String.format("%.1f, %.2f, %+.2f, %s", args[0], args[0], args[0], bin);
@@ -36,6 +57,16 @@ public class Task017Impl implements Task017 {
      */
     @Override
     public String formatDates(Object... args) {
+        if (args == null || args.length == 0) {
+            throw new IllegalArgumentException();
+        }
+
+        for (Object each : args) {
+            if (each == null) {
+                throw new IllegalArgumentException();
+            }
+        }
+
         final Date targetDate = new Date();
         final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy.dd.MM");
         System.out.println(dateFormat.format(targetDate));
