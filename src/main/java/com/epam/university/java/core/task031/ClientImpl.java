1e680237db8b0b66e62d374beee7ebf31418ec03
Pavel <gunpavel.i@gmail.com>
2020-09-08 13:52:45 +0300

    feat: Task031

--- src/main/java/com/epam/university/java/core/task031/ClientImpl.java
+++ src/main/java/com/epam/university/java/core/task031/ClientImpl.java
@@ -1,2 +1,43 @@
-package com.epam.university.java.core.task031;public class ClientImpl {
+package com.epam.university.java.core.task031;
+
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+
+public class ClientImpl implements Client {
+    private Socket serverSocket;
+    private BufferedWriter out;
+
+    @Override
+    public void sendMessage(String message) {
+        try {
+            out.write(message + "\n");
+            out.flush();
+            Thread.sleep(1000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void start() {
+        try {
+            serverSocket = new Socket("localhost", 6666);
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(
+                    serverSocket.getOutputStream());
+            out = new BufferedWriter(outputStreamWriter);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void stop() {
+        try {
+            serverSocket.close();
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/com/epam/university/java/core/task031/ServerImpl.java
+++ src/main/java/com/epam/university/java/core/task031/ServerImpl.java
@@ -1,2 +1,60 @@
-package com.epam.university.java.core.task031;public class ServerImpl {
+package com.epam.university.java.core.task031;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.LinkedList;
+
+public class ServerImpl implements Server {
+    private boolean currentThreadIsWorking = true;
+    private ServerSocket serverSocket;
+    private final LinkedList<String> strings = new LinkedList<>();
+
+    @Override
+    public String readMessage() {
+        if (strings.isEmpty()) {
+            return "";
+        } else {
+            return strings.removeLast();
+        }
+    }
+
+    @Override
+    public void start() {
+        Thread thread = new Thread(() -> {
+            try {
+                serverSocket = new ServerSocket(6666);
+                while (true) {
+                    Socket clientSocket = serverSocket.accept();
+                    new Thread(() -> {
+                        try (BufferedReader in = new BufferedReader(
+                                new InputStreamReader(clientSocket.getInputStream()))) {
+                            while (currentThreadIsWorking) {
+                                if (in.ready()) {
+                                    strings.add(in.readLine());
+                                }
+                            }
+
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }).start();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        thread.start();
+    }
+
+    @Override
+    public void stop() {
+        currentThreadIsWorking = false;
+        try {
+            serverSocket.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
