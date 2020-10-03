4dec4fba86683144160700679e9330e67fd83205
Pavel <gunpavel.i@gmail.com>
2020-10-03 16:59:21 +0300

    feat: context

---
+++ src/main/java/com/epam/university/java/project/core/cdi/bean/BeanDefinitionRegistryImpl.java
@@ -0,0 +1,2 @@
+package com.epam.university.java.project.core.cdi.bean;public class BeanDefinitionRegistryImpl {
+}
---
+++ src/main/java/com/epam/university/java/project/core/cdi/structure/MapDefinitionImpl.java
@@ -0,0 +1,2 @@
+package com.epam.university.java.project.core.cdi.structure;public class MapDefinitionImpl {
+}
