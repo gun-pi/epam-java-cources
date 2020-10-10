df0e23d0b2be181dd1a0942bae1cf2fbf47bf4c3
Pavel <gunpavel.i@gmail.com>
2020-10-10 12:44:44 +0300

    feat: project.domain + fix: project.core

--- src/main/java/com/epam/university/java/project/service/BookDaoXmlImpl.java
+++ src/main/java/com/epam/university/java/project/service/BookDaoXmlImpl.java
@@ -1,2 +1,74 @@
-package com.epam.university.java.project.service;public class BookDaoXmlImpl {
+package com.epam.university.java.project.service;
+
+import com.epam.university.java.project.domain.Book;
+import com.epam.university.java.project.domain.BookImpl;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class BookDaoXmlImpl implements BookDao {
+    private Collection<Book> books;
+    private int bookId;
+
+    public BookDaoXmlImpl() {
+        books = new ArrayList<>();
+    }
+
+
+    /**
+     * Create new book instance.
+     *
+     * @return new book instance
+     */
+    @Override
+    public Book createBook() {
+        return new BookImpl();
+    }
+
+    /**
+     * Get the book by id.
+     *
+     * @param id book id
+     * @return book instance
+     */
+    @Override
+    public Book getBook(int id) {
+        return books.stream()
+                .filter(n -> n.getId() == id)
+                .findAny()
+                .orElse(null);
+    }
+
+    /**
+     * Get all books.
+     *
+     * @return books collection
+     */
+    @Override
+    public Collection<Book> getBooks() {
+        return books;
+    }
+
+    /**
+     * Remove book from library.
+     *
+     * @param book book to remove
+     */
+    @Override
+    public void remove(Book book) {
+        books.remove(book);
+    }
+
+    /**
+     * Save book to library.
+     *
+     * @param book book to save
+     * @return saved book instance
+     */
+    @Override
+    public Book save(Book book) {
+        book.setId(++bookId);
+        books.add(book);
+        return book;
+    }
 }
