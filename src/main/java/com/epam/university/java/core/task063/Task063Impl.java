723088edfc41266a75e8d573d54ff4cb5f03be9f
Pavel <gunpavel.i@gmail.com>
2020-11-13 07:26:38 +0300

    feat: task063

---
+++ src/main/java/com/epam/university/java/core/task063/Task063Impl.java
@@ -0,0 +1,30 @@
+package com.epam.university.java.core.task063;
+
+public class Task063Impl implements Task063 {
+    /**
+     * Calculate digital root of given number.
+     *
+     * @param number given number for calculation
+     * @return digital root
+     * @throws IllegalArgumentException if number not provided
+     */
+    @Override
+    public Integer calcDigitalRoot(Integer number) {
+        if (number == null) {
+            throw new IllegalArgumentException();
+        }
+
+        while(true) {
+            if (String.valueOf(number).length() == 1) {
+                break;
+            }
+            int result = 0;
+            for (String digit : String.valueOf(number).split("")) {
+                result += Integer.parseInt(digit);
+            }
+            number = result;
+        }
+
+        return number;
+    }
+}
